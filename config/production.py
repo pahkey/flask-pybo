2b3eae0c18f5dfff7aa27c1cbbcb60209a790abf
박응용 <pahkey@gmail.com>
2020-06-25 10:04:43 +0900

    logging

--- config/production.py
+++ config/production.py
@@ -7,6 +7,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
 
+"""
 dictConfig({
     'version': 1,
     'formatters': {
@@ -34,3 +35,4 @@ dictConfig({
         'handlers': ['wsgi']
     }
 })
+"""
