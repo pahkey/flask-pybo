6c50ad0ce8eb9ced500bd1bdb9235bfa409237c9
박응용 <pahkey@gmail.com>
2020-06-25 10:08:50 +0900

    loggign

--- config/production.py
+++ config/production.py
@@ -1,4 +1,3 @@
-import os
 from logging.config import dictConfig
 
 from config.default import *
@@ -7,7 +6,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
 
-"""
 dictConfig({
     'version': 1,
     'formatters': {
@@ -24,8 +22,8 @@ dictConfig({
         'file': {
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': os.path.join(BASE_DIR, 'logs/mysite.log'),
-            'maxBytes': 1024*1024*5,  # 5 MB
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'default',
         },
@@ -35,4 +33,3 @@ dictConfig({
         'handlers': ['wsgi']
     }
 })
-"""
